0fd321e77fd472697ec829cb79b1965979ca6f2b
arsmn <arsmn1996@gmail.com>
2020-05-09 03:05:33 +0430

    Add url rewriting

--- main.go
+++ main.go
@@ -5,6 +5,10 @@
 package proxy
 
 import (
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/gofiber/fiber"
@@ -29,31 +33,103 @@ func releaseHostClient(hc *fasthttp.HostClient) {
 	poolHostClient.Put(hc)
 }
 
-// New returns a new reverse proxy handler
-func New(target string) func(*fiber.Ctx) {
-	// Must provide a target
-	if target == "" {
-		panic("You must provide a backend server <host>:<port>")
+// Config holds configuration for the middleware
+type Config struct {
+	// DownstreamHosts is list of backend hosts used to proxy the request.
+	// Backend hosts is selected by Round-Robin scheduling.
+	// Required. Default: nil
+	DownstreamHosts []string
+
+	// UpstreamMethods is list of HTTP methods allowed for proxying.
+	// Optional. Default: nil
+	UpstreamMethods []string
+
+	// Filter defines a function to skip middleware.
+	// Optional. Default: nil
+	Filter func(*fiber.Ctx) bool
+
+	// ErrorHandler is a function for handling unexpected errors.
+	// Optional. Default: StatusServiceUnavailable
+	ErrorHandler func(*fiber.Ctx, error)
+
+	// Rules defines the URL path rewrite rules. The values captured in asterisk can be
+	// retrieved by index e.g. $1, $2 and so on.
+	// Required. Example:
+	// "/old":              "/new",
+	// "/api/*":            "/$1",
+	// "/js/*":             "/public/javascripts/$1",
+	// "/users/*/orders/*": "/user/$1/order/$2",
+	Rules map[string]string
+
+	rulesRegex map[*regexp.Regexp]string
+}
+
+// New returns a new reverse proxy middleware
+func New(config ...Config) func(*fiber.Ctx) {
+
+	var cfg Config
+	if len(config) > 0 {
+		cfg = config[0]
 	}
-	// Return middleware handler
-	return func(c *fiber.Ctx) {
-		// Get new hostclient from pool and release when done
-		client := acquireHostClient()
-		defer releaseHostClient(client)
-		// Adjust hostclient settings
-		client.Addr = target
-		// Prepare request
-		req := &c.Fasthttp.Request
-		resp := &c.Fasthttp.Response
-		// Alter request and remove unneeded headers from request
-		req.Header.Del("Connection")
-		// Make request to upstream host and handle erropr
-		if err := client.Do(req, resp); err != nil {
-			c.SendStatus(503) // Service Unavailble
+
+	if len(cfg.DownstreamHosts) == 0 {
+		log.Fatal("Fiber: Proxy middleware requires at least one downstream target")
+	}
+
+	if cfg.ErrorHandler == nil {
+		cfg.ErrorHandler = func(c *fiber.Ctx, err error) {
+			c.SendStatus(fiber.StatusServiceUnavailable)
 			return
 		}
-		// Alter request and remove unneeded headers from response
-		resp.Header.Del("Connection")
+	}
+
+	cfg.rulesRegex = map[*regexp.Regexp]string{}
+	for k, v := range cfg.Rules {
+		k = strings.Replace(k, "*", "(.*)", -1)
+		k = k + "$"
+		cfg.rulesRegex[regexp.MustCompile(k)] = v
+	}
+
+	return func(c *fiber.Ctx) {
+
+		if cfg.Filter != nil && cfg.Filter(c) {
+			c.Next()
+			return
+		}
+
+		for _, method := range cfg.UpstreamMethods {
+			if c.Method() != strings.ToUpper(method) {
+				c.Next()
+				return
+			}
+		}
+
+		req := &c.Fasthttp.Request
+		resp := &c.Fasthttp.Response
+
+		for k, v := range cfg.rulesRegex {
+			replacer := captureTokens(k, c.Path())
+			if replacer != nil {
+				req.SetRequestURI(replacer.Replace(v))
+			}
+		}
+
+		target := strings.Join(cfg.DownstreamHosts, ",")
+		if err := proxy(req, resp, target); err != nil {
+			cfg.ErrorHandler(c, err)
+		}
+		return
+	}
+}
+
+// Handler returns a reverse proxy handler
+func Handler(target string) func(*fiber.Ctx) {
+	if target == "" {
+		log.Fatal("Fiber: Proxy middleware requires backend server <host>:<port>")
+	}
+
+	return func(c *fiber.Ctx) {
+		Forward(c, target)
 	}
 }
 
@@ -63,23 +139,47 @@ func Forward(c *fiber.Ctx, target string) error {
 	if target == "" {
 		panic("You must provide a backend server <host>:<port>")
 	}
+
+	req := &c.Fasthttp.Request
+	resp := &c.Fasthttp.Response
+
+	if err := proxy(req, resp, target); err != nil {
+		c.SendStatus(fiber.StatusServiceUnavailable)
+		return err
+	}
+	return nil
+}
+
+func proxy(req *fasthttp.Request, resp *fasthttp.Response, target string) error {
 	// Get new hostclient from pool and release when done
 	client := acquireHostClient()
 	defer releaseHostClient(client)
 	// Adjust hostclient settings
 	client.Addr = target
-	// Prepare request
-	req := &c.Fasthttp.Request
-	resp := &c.Fasthttp.Response
 	// Alter request and remove unneeded headers from request
 	req.Header.Del("Connection")
-	// Make request to upstream host and handle erropr
+	// Make request to upstream host and handle error
 	if err := client.Do(req, resp); err != nil {
-		c.SendStatus(503) // 503 Service Unavailble
 		return err
 	}
 	// Alter request and remove unneeded headers from response
 	resp.Header.Del("Connection")
 	// Return without errors
 	return nil
+}
+
+// https://github.com/labstack/echo/blob/master/middleware/rewrite.go
+func captureTokens(pattern *regexp.Regexp, input string) *strings.Replacer {
+	groups := pattern.FindAllStringSubmatch(input, -1)
+	if groups == nil {
+		return nil
+	}
+	values := groups[0][1:]
+	replace := make([]string, 2*len(values))
+	for i, v := range values {
+		j := 2 * i
+		replace[j] = "$" + strconv.Itoa(i+1)
+		replace[j+1] = v
+	}
+	return strings.NewReplacer(replace...)
 }
